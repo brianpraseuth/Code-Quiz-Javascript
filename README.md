eeed0722473c7756d4b915d3dab0dd0afdfd3992
Brian Praseuth <brianpraseuth@gmail.com>
2020-07-28 23:11:36 -0700

    Assignment done to the best of my ability. Functioning quiz with timer, but missing high score feature.

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Code-Quiz-Javascript
+The goal of the assignment was to create a working application in the form of a quiz. I found some parts of the criteria quite easy to achieve, but unfortunately found a lot of difficulty in certain parts like the end screen/high school page. I understand i was to use the set local storage method, but couldn't quite grasp how to get it functioning. Although the assignment isn't fully completed to my liking I'm happy with how far I've gotten overall. 
+
+
+
+## Acceptance Criteria
+
+```
+GIVEN I am taking a code quiz
+WHEN I click the start button
+THEN a timer starts and I am presented with a question
+WHEN I answer a question
+THEN I am presented with another question
+WHEN I answer a question incorrectly
+THEN time is subtracted from the clock
+WHEN all questions are answered or the timer reaches 0
+THEN the game is over
+WHEN the game is over
+THEN I can save my initials and score.
+
---
+++ script.js
@@ -0,0 +1,117 @@
+// Create variables for everything needed to make quiz function correctly.
+var quizContainer = document.getElementById("quizArea");
+var resultsContainer = document.getElementById("answerResults");
+var startBtn = document.getElementById("startButton");
+var timerDiv = document.getElementById("timeLeft");
+var correctIncorrect = document.getElementById("feedback");
+var questionOptions = document.getElementById("question");
+var answerOptions = document.getElementById("answers");
+var startPage = document.getElementById("startingPage");
+var quizOver = document.getElementById("endGame");
+
+// Timer of 90 seconds that starts when "Start Quiz" button is clicked.
+var secondsLeft = 90;
+
+function startTime() {
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    timerDiv.textContent = "Time Remaining: " + secondsLeft;
+
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      endQuiz();
+    }
+
+  }, 1000);
+}
+
+// Array containing questions and answers for the quiz.
+var quizQuestions = [
+    {
+      question: "Commonly used data types DO NOT include:",
+      answers: ["strings", "booleans", "alerts", "numbers"],
+      correctAnswer: "alerts"
+    },
+
+    {
+      question: "The condition in an if / else statement is enclosed within ______.",
+      answers: ["quotes", "curly brackets", "parantheses", "square brackets"],
+      correctAnswer: "parantheses"
+    },
+
+    {
+      question: "Arrays in JavaScript can be used to store ______.",
+      answers: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      correctAnswer: "all of the above"
+    },
+
+    {
+      question: "String values must be enclosed within ______ when being assigned to variables.",
+      answers: ["commas", "curly brackets", "quotes", "parentheses"],
+      correctAnswer: "quotes"
+    },
+
+    {
+      question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      answers: ["javascript", "terminal/bash", "for loops", "console.log"],
+      correctAnswer: "console.log"
+    },
+  ];
+
+  var currentIndex = 0;
+
+  function startQuiz() {
+    var currentQuestion = quizQuestions[currentIndex];
+    questionOptions.textContent = currentQuestion.question;
+    console.log(currentQuestion.answers);
+    answerOptions.textContent = "";
+    for (var i = 0; i < currentQuestion.answers.length; i++) {
+      var item = document.createElement("li");
+      var answer = document.createElement("button");
+      answer.setAttribute("value", currentQuestion.answers[i]);
+      answer.setAttribute("class", "btn btn-dark");
+      answer.onclick = checkAnswer;
+      answer.textContent = currentQuestion.answers[i];
+      item.append(answer);
+      answerOptions.append(item);
+      console.log(currentQuestion.answers[i]);
+    }
+  }
+
+  function setScreen() {
+    startPage.classList.add("hide");
+    quizContainer.classList.remove("hide");
+    quizOver.classList.add("hide");
+    startQuiz();
+    startTime();
+  }
+
+  function checkAnswer() {
+    if (this.value === quizQuestions[currentIndex].correctAnswer) {
+      currentIndex++;
+      startQuiz();
+      var correct = document.createElement("p");
+      resultsContainer.textContent = "Correct!";
+
+    } else {
+      currentIndex++;
+      secondsLeft -= 10;
+      timerDiv.textContent = "Time Remaining: " + secondsLeft;
+      startQuiz();
+      var correct = document.createElement("p");
+      resultsContainer.textContent = "Incorrect!";
+    }
+
+  }
+
+  function endQuiz(){
+    if(secondsLeft === 0) {
+      clearInterval(timerInterval);
+      startPage.classList.add("hide");
+      quizContainer.classList.add("hide");
+      quizOver.classList.remove("hide");
+    }
+  }
+startBtn.addEventListener("click", function() {
+  setScreen();
+});
